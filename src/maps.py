87331e717c309c7e8c9a9131856709870bf7bba6
Krishna Agarwal <krishna1991krish@gmail.com>
2020-04-02 17:21:38 -0700

    Report Update of  04/02/2020

--- src/maps.py
+++ src/maps.py
@@ -10,7 +10,17 @@ from src import CurrentDate
 import warnings
 warnings.filterwarnings("ignore")
 
-def world_mapping(COVID_DATA_df,maptype):
+def mapping_plot(TYPE_DF, df, maptype, title_desc):
+    fig, ax = plt.subplots(1, figsize=(20,8.5))
+    ax.axis('off')
+    ax.set_title(title_desc)
+    TYPE_DF.plot(ax=ax, column=maptype, legend=True,scheme='fisher_jenks')
+    
+    #Emptying memory
+    df = 0
+    TYPE_DF = 0
+
+def mapping_world(COVID_DATA_df,maptype):
     df = daf.country_wise_count(COVID_DATA_df)
     df.columns = df.columns.str.replace('Country/Region', 'COUNTRY')
     df['COUNTRY'] = df['COUNTRY'].replace('US','United States')
@@ -21,14 +31,9 @@ def world_mapping(COVID_DATA_df,maptype):
     world_country = gpd.read_file('./SHP-Files/World/World_Countries.shp')
     world_country = world_country[(world_country['COUNTRY'] != 'Antarctica')]
     world_country = pd.merge(world_country, df, on='COUNTRY', how='inner')
-    fig, ax = plt.subplots(1, figsize=(20,8.5))
-    ax.axis('off')
-    world_country.plot(ax=ax, column='Confirmed', legend=True)
+    title_desc = str(maptype + ' cases of Corona Virus in the World as on ' + CurrentDate.DATE_UPDATE)
+    mapping_plot(world_country, df,maptype, title_desc)
     
-    #Emptying memory
-    df = 0
-    world_country = 0
-
 def mapping_usa(US_COVID_DATA_df, maptype):
     df = US_COVID_DATA_df[US_COVID_DATA_df['Last Update'].str.contains(CurrentDate.DATE_UPDATE)]
     df = df[['FIPS','Confirmed','Province/State','Deaths','Recovered']]
@@ -48,11 +53,34 @@ def mapping_usa(US_COVID_DATA_df, maptype):
     projection = "+proj=laea +lat_0=30 +lon_0=-95"
     us_states = us_states.to_crs(projection)
     us_states = pd.merge(us_states, df, on='NAME', how='inner')
-    fig, ax = plt.subplots(1, figsize=(15,8.5))
-    ax.axis('off')
-    ax.set_title(maptype + ' cases of Corona Virus in US as on ' + CurrentDate.DATE_UPDATE)
-    us_states.plot(ax=ax, column=maptype, legend=True)
+    title_desc = str(maptype + ' cases of Corona Virus in US as on ' + CurrentDate.DATE_UPDATE)
+    mapping_plot(us_states, df,maptype, title_desc)
+
+def mapping_europe(COVID_DATA_df, maptype):
+    df = daf.country_wise_count(COVID_DATA_df)
+    df.columns = df.columns.str.replace('Country/Region', 'NAME')
+
+    europe_continent = gpd.read_file('./SHP-Files/Europe/Europe.shp')
+    europe_continent = pd.merge(europe_continent, df, on='NAME', how='inner')
+    title_desc = str(maptype + ' cases of Corona Virus in Europe as on ' + CurrentDate.DATE_UPDATE)
+    mapping_plot(europe_continent, df,maptype, title_desc)
     
-    #Emptying memory
-    df = 0
-    us_states = 0
+def mapping_australia(COVID_DATA_df, maptype):
+    df = COVID_DATA_df[COVID_DATA_df['Last Update'].str.contains(CurrentDate.DATE_UPDATE)]
+    df = df[['Confirmed','Province/State','Deaths','Recovered']]
+    df.columns = df.columns.str.replace('Province/State', 'NAME')
+    
+    australia_continent = gpd.read_file('./SHP-Files/Australia/Australia.shp')
+    australia_continent = pd.merge(australia_continent, df, on='NAME', how='inner')
+    title_desc = str(maptype + ' cases of Corona Virus in Australia as on ' + CurrentDate.DATE_UPDATE)
+    mapping_plot(australia_continent, df,maptype, title_desc)
+
+def mapping_americas(COVID_DATA_df, maptype):
+    df = daf.country_wise_count(COVID_DATA_df)
+    df.columns = df.columns.str.replace('Country/Region', 'COUNTRY')
+    df['COUNTRY'] = df['COUNTRY'].replace('US','United States')
+    
+    americas_continent = gpd.read_file('./SHP-Files/Americas/Americas.shp')
+    americas_continent = pd.merge(americas_continent, df, on='COUNTRY', how='inner')
+    title_desc = str(maptype + ' cases of Corona Virus in Americas as on ' + CurrentDate.DATE_UPDATE)
+    mapping_plot(americas_continent, df,maptype, title_desc)
